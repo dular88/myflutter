b02a95f7846eff22aa8eb863b8e74becff57a6e2
dular88 <dular88@gmail.com>
2022-07-25 19:25:20 +0530

    flutter tutorial day2

--- fluttertutorial/lib/main.dart
+++ fluttertutorial/lib/main.dart
@@ -1,22 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertutorial/pages/home_page.dart';
+import 'package:fluttertutorial/pages/login_page.dart';
 
 void main() {
   runApp(MyFlutters());
 }
 
 class MyFlutters extends StatelessWidget {
-  const MyFlutters({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text('Welcome Flutter'),
-          ),
-        ),
-      ),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(brightness: Brightness.light),
+      initialRoute: '/home',
+      routes: {
+        '/': (context) => LoginPage(),
+        '/home': (context) => HomePage(),
+        '/login': (context) => LoginPage()
+      },
     );
   }
 }
---
+++ fluttertutorial/lib/pages/home_page.dart
@@ -0,0 +1,22 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  final int age = 34;
+  final String name = 'Dinesh';
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Catalog App'),
+      ),
+      body: Center(
+        child: Container(
+          child: Text('Testing Catalog App'),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
